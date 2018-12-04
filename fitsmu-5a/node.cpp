b0082e4c3487399d2c033f3b7be473b043a5ced2
Jason Fitch <jasonpfitch@gmail.com>
2018-12-04 16:19:24 -0500

    migrating node class

---
+++ fitsmu-5a/node.cpp
@@ -0,0 +1,136 @@
+/**
+ * TEAM: fitsmu
+ * Jason Fitch
+ * Sam Smucny
+ *
+ * node.cpp: contains all of the implementations for member methods in the
+ *           node.h file
+ */
+
+node::node()
+// Constructor, creates a new, uninitialized node. id is initialized
+// elsewhere (by the graph constructor).
+{
+   unMark();
+   unVisit();
+   setWeight(0);
+}
+
+node::node(const node &n)
+// Copy constructor
+//
+// const node n: the node object whose data is being copied into this node
+{
+   setNode(n.getId(), n.getWeight(), n.isMarked(), n.isVisited());
+}
+
+node &node::operator=(const node &n)
+// Overloaded assignment operator.
+//
+// const node n: the node object whose data is being used to create this object
+// A shallow copy of this object is returned
+{
+   setNode(n.getId(), n.getWeight(), n.isMarked(), n.isVisited());
+   return *this;
+}
+
+NodeWeight node::getWeight() const
+// Return node's weight
+{
+   return weight;
+}
+
+void node::setWeight(NodeWeight w)
+// Set node's weight to w.
+//
+// NodeWeight w: the new weight of this node
+{
+   weight = w;
+}
+
+void node::setId(int i)
+// Set node's id to i.  Throws an exception if i < 0.
+//
+// int i: this node's id
+{
+   if (i < 0)
+      throw rangeError("Bad value in node::setId");
+
+   id = i;
+}
+
+int node::getId() const
+// Return node's id
+{
+   return id;
+}
+
+void node::setNode(int id, NodeWeight w = 0, bool m = false, bool v = false)
+// Initialize a node
+//
+// int id: the new id of this node
+// NodeWeight w: the new weight of this node. If passed nothing, the new weight
+//               is 0.
+// bool m: The boolean value determining if this node has been marked. Default
+//         to false
+// bool v: The boolean value determining if this node has been visited. Default
+//         to false
+{
+   setId(id);
+   setWeight(w);
+
+   if (m)
+      mark();
+   else
+      unMark();
+
+   if (v)
+      visit();
+   else
+      unVisit();
+}
+
+void node::mark()
+   // Mark node.
+{
+   marked = true;
+}
+
+void node::unMark()
+   // Un-mark node.
+{
+   marked = false;
+}
+
+bool node::isMarked() const
+   // Return true if node is marked, and false otherwise.
+{
+   return marked;
+}
+
+void node::visit()
+   // Set visited to true;
+{
+   visited = true;
+}
+
+void node::unVisit()
+   // Set visited to false;
+{
+   visited = false;
+}
+
+bool node::isVisited() const
+   // Return true if node is visited, and false otherwise.
+{
+   return visited;
+}
+
+
+ostream &operator<<(ostream &ostr, const node &n)
+{
+   ostr << "node: " << n.getId() << " weight: " << n.getWeight()
+        << " visited: " << n.isVisited() << " marked " << n.isMarked() << endl;
+
+   return ostr;
+}
---
+++ fitsmu-5a/node.h
@@ -0,0 +1,48 @@
+/**
+ * TEAM: fitsmu
+ * Jason Fitch
+ * Sam Smucny
+ *
+ * node.h: Contains the class definitions of the node, including the private
+ *         data members and public methods
+ */
+
+#include <iostream>
+#include <fstream>
+#include "d_except.h"
+
+using namespace std;
+typedef int NodeWeight;
+
+// Represents the node class
+// Contains the unique identification of the node, the weight of the node,
+// whether the node has been marked, and whether the node has been visited
+class node
+{
+   public:
+      node();
+      node(const node &);
+      node &operator=(const node &);
+
+      void setId(int i);
+      int getId() const;
+
+      void setWeight(NodeWeight);
+      NodeWeight getWeight() const;
+
+      void setNode(int, NodeWeight, bool, bool);
+
+      void mark();
+      void unMark();
+      bool isMarked() const;
+
+      void visit();
+      void unVisit();
+      bool isVisited() const;
+
+   private:
+      int id;
+      NodeWeight weight;
+      bool marked;
+      bool visited;
+};
